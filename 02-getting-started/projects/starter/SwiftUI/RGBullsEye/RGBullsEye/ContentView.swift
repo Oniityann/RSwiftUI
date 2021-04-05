9a52a12935e77d84f87aaca1bdffba0c24bfe3ba
Oniityann <zynfab26@hotmail.com>
2021-04-05 17:03:22 +0800

    update 1st courses
    State & Binding

--- 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
+++ 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
@@ -33,14 +33,39 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State var game = Game()
+  @State var guess: RGB
   var body: some View {
-    Text("Hello, world!")
+    VStack {
+      Color(rgbStruct: game.target)
+      Text("R: ??? G: ??? B: ???")
+        .padding()
+      
+      Color(rgbStruct: guess)
+      Text(
+        "R: \(Int(guess.red * 255.0))"
+          + "  G: \(Int(guess.green * 255.0))"
+          + "  B: \(Int(guess.blue * 255.0))"
+      )
       .padding()
+      
+      HStack {
+        Text("0")
+        Slider(value: $guess.red)
+          .accentColor(.red)
+        Text("255")
+      }
+      .padding(.horizontal)
+      
+      Button(action: {}) {
+        Text("HIT ME!")
+      }
+    }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+    ContentView(guess: RGB(red: 0.8, green: 0.3, blue: 0.7))
   }
 }
