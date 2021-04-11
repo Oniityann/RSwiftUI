2441141052275138d68b876b2d568ad313fb3e5c
ZhengYinan <zynfab26@hotmail.com>
2021-04-12 06:08:20 +0800

    11
    11

--- 06-controls-and-user-input/projects/Kuchi/Kuchi/Welcome/RegisterView.swift
+++ 06-controls-and-user-input/projects/Kuchi/Kuchi/Welcome/RegisterView.swift
@@ -7,15 +7,11 @@
 
 import SwiftUI
 
-struct RegisterView: View {
-  @State var name = ""
-  var body: some View {
-    VStack {
-      Spacer()
-      
-      WelcomeMessageView()
-      
-      TextField("Type your name...", text: $name)
+
+struct KuchiTextStyle: TextFieldStyle {
+  public func _body(
+    configuration: TextField<Self._Label>) -> some View {
+      return configuration
         .padding(EdgeInsets(top: 8, leading: 16,
                             bottom: 8, trailing: 16))
         .background(Color.white)
@@ -27,7 +23,19 @@ struct RegisterView: View {
         .shadow(color: Color.gray.opacity(0.4),
                 radius: 3, x: 1, y: 2)
         .cornerRadius(8)
+  }
+}
 
+struct RegisterView: View {
+  @State var name = ""
+  var body: some View {
+    VStack {
+      Spacer()
+      
+      WelcomeMessageView()
+      
+      TextField("Type your name...", text: $name)
+        .textFieldStyle(KuchiTextStyle())
       
       Spacer()
     }
