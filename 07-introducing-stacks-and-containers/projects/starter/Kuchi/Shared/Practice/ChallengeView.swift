58857192e455dd2bbd99a6f7b24d3c0ac3af1bae
ZhengYinan <zynfab26@hotmail.com>
2021-04-28 19:44:16 +0800

    challengeview

--- 07-introducing-stacks-and-containers/projects/starter/Kuchi/Shared/Practice/ChallengeView.swift
+++ 07-introducing-stacks-and-containers/projects/starter/Kuchi/Shared/Practice/ChallengeView.swift
@@ -33,14 +33,46 @@
 import SwiftUI
 
 struct ChallengeView: View {
+  let challengeTest: ChallengeTest
+  @State var showAnswers = false
+
   var body: some View {
-    Text("Hello World!")
+    // 1
+    VStack {
+      // 2
+      Button(action: {
+        self.showAnswers.toggle()
+      }) {
+        // 3
+        QuestionView(question: challengeTest.challenge.question)
+          .frame(height: 300)
+      }
+
+      // 4
+      if showAnswers {
+        Divider()
+        // 5
+        ChoicesView(challengeTest: challengeTest)
+          .frame(height: 300)
+          .padding()
+      }
+    }
   }
 }
 
-
 struct ChallengeView_Previews: PreviewProvider {
+  // 1
+  static let challengeTest = ChallengeTest(
+    challenge: Challenge(
+      question: "おねがい　します",
+      pronunciation: "Onegai shimasu",
+      answer: "Please"
+    ),
+    answers: ["Thank you", "Hello", "Goodbye"]
+  )
+  
   static var previews: some View {
-    return ChallengeView()
+    // 2
+    return ChallengeView(challengeTest: challengeTest)
   }
 }
